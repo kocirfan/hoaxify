74b23734015a4cee15e69b8d5c903554fad2649f
kocirfan <irfnk83@gmail.com>
2022-04-29 05:34:58 +0300

    uygulamamıza dil seçeneği eklemek

--- src/main/java/com/kocirfan/user/User.java
+++ src/main/java/com/kocirfan/user/User.java
@@ -18,7 +18,7 @@ public class User {
     @GeneratedValue
     private Long id;
 
-    @NotNull
+    @NotNull(message = "{hoaxify.constraint.username.NotNull.message}")
     @Size(min = 4, max = 255)
     @UniqueUsername
     private String username;
@@ -29,7 +29,7 @@ public class User {
 
     @NotNull
     @Size(min = 8, max = 255)
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$", message = "{hoaxify.constraint.password.Pattern.message}")
     private String password;
 
 
